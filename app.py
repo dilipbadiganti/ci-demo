9055791914895b54da3673520cc92a8ac3087fb0
dilipx <dilipbadiganti@gmail.com>
2025-11-20 18:25:36 +0530

    break app.py on purpose

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from CI/CD pipeline!"
+    return "Hello brokie CI/CD pipeline!"
 
 if __name__ == "__main__":
     app.run(debug=True)
